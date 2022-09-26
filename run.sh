d6353ff73009e40b9710e1c8832fdda22152602e
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2022-09-26 15:16:43 +1000

    changed order

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@ set -ex
 
 while true ;
 do
-nix build github:nixos/nixpkgs/staging-next#awsebcli
 nix build github:nixos/nixpkgs/master#awsebcli
-nix build github:nixos/nixpkgs/staging#awsebcli
+nix build github:nixos/nixpkgs/staging-next#awsebcli
 nix build github:nixos/nixpkgs/python-updates#awsebcli
+nix build github:nixos/nixpkgs/staging#awsebcli
 sleep 1000
 done
